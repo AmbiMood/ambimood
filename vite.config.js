da3fbcbe9596e5f8ed2221a1f618f96a9947ee81
AmbiMood <ambimood2026@gmail.com>
2026-03-09 13:49:32 +0530

    fix: vite proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://ambimood-backend-2.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
